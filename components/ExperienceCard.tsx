75d366dcc76258a1a2fb3abaa071c47c3568f8ff
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-09-21 00:54:52 +0100

    Added Experience component

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,14 +1,42 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 type Props = {}
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article className=''>
-        <img 
+    <article className='flex flex-col items-center flex-shrink-0 rounded-lg space-y-7 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+        <motion.img
+            initial={{
+                y: -100,
+                opacity: 0,
+            }}
+            transition={{ duration: 1.2 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
             src="/images/efosa_igbinehi.jpg" alt="company logo"
             className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
         />
+
+        <div className='px-0 md:px-10'>
+            <h4 className='text-3xl font-light'>Senior Software Developer</h4>
+            <h5 className='mt-1 text-2xl font-bold'>Zaizi, London</h5>
+            <aside className='flex my-2 space-x-2'>
+                <img src="images/python.png" alt="" className='w-10 h-10 rounded-full' />
+                <img src="images/python.png" alt="" className='w-10 h-10 rounded-full' />
+                <img src="images/python.png" alt="" className='w-10 h-10 rounded-full' />
+                <img src="images/python.png" alt="" className='w-10 h-10 rounded-full' />
+            </aside>
+            <p className='py-5 text-gray-300 uppercase'>Started work... - Ended...</p>
+
+            <ul className='ml-5 space-y-2 text-lg list-disc'>
+                <li>Summary point</li>
+                <li>Summary point</li>
+                <li>Summary point</li>
+                <li>Summary point</li>
+                <li>Summary point</li>
+            </ul>
+        </div>
     </article>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory z-0 overflow-scroll'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory z-0'>
       {/* <Head>
         <title>Efosa Igbinehi | Home</title>
         <meta name="description" content="A fullstack developer from Manchester, UK. Building bespoke user experiences since 2014." />
